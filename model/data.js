088222b48a547b4fac459847ba62c2df98ab79a3
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-01-12 12:28:45 +0530

    Initial commit

---
+++ model/data.js
@@ -0,0 +1,39 @@
+const songs = [
+  {
+    title: 'Treat you Better',
+    artist: 'Shawn Mendes',
+    image: require('../assets/covers/treat_you_better.jpg'),
+    url: require('../assets/songs/treat_you_better.mp3'),
+    id: '1',
+  },
+  {
+    title: 'Perfect',
+    artist: 'Ed Sheeran',
+    image: require('../assets/covers/perfect.jpg'),
+    url: require('../assets/songs/perfect.mp3'),
+    id: '2',
+  },
+  {
+    title: "We don't talk anymore",
+    artist: 'Charlie Puth',
+    image: require('../assets/covers/we_dont_talk_anymore.jpg'),
+    url: require('../assets/songs/we_dont_talk_anymore.mp3'),
+    id: '3',
+  },
+  {
+    title: 'Despacito',
+    artist: 'Luis Fonsi',
+    image: require('../assets/covers/despacito.jpg'),
+    url: require('../assets/songs/despacito.mp3'),
+    id: '4',
+  },
+  {
+    title: "I don't wanna live forever",
+    artist: 'Zayn Malik',
+    image: require('../assets/covers/i_dont_wanna_live_forever.jpg'),
+    url: require('../assets/songs/i_dont_wanna_live_forever.mp3'),
+    id: '5',
+  },
+];
+
+export default songs;
